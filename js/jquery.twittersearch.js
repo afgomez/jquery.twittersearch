08fee355759d869a3783732ae36444d0f689ee4c
Alejandro Fernández <antarticonorte@gmail.com>
2009-04-04 01:09:28 +0200

    Basic search functionality

--- js/jquery.twittersearch.js
+++ js/jquery.twittersearch.js
@@ -8,12 +8,64 @@
     $.fn.twitterSearch = function(query_string, options) {
         var opts = $.extend({}, $.fn.twitterSearch.defaults, options);
         
+        var jObject = this; // We will use this for the success and error callbacks
+        
+        // Do the Twitter API call
+        $.ajax({
+            url: 'http://search.twitter.com/search.json',
+            data: {
+                q: query_string,
+                rpp: opts.results
+            },
+            dataType: 'jsonp',
+            success: function(data, status) {
+                renderInto(jObject, data);
+            },
+            error: function() {
+                // TODO
+            }
+        });
+        
+        return this;
     };
     
     // Default settings
     $.fn.twitterSearch.defaults = {
-    
+        results: 20
     };
     
+    
+    /**
+     * Renders the data returned from Twitter into each jQuery object
+     */
+    function renderInto(jObject, data) {
+        jObject.each(function() {
+            
+            if (data.results.length == 0) {
+                $('<div>')
+                    .addClass('tweetersearch-info')
+                    .html('No matches for ' + data.query)
+                    .appendTo(this);
+                
+                return;
+            }
+            
+            if (this.tagName.toLowerCase() != 'ul') {
+                var list = $('<ul>').appendTo(this);
+            }
+            else {
+                var list = $(this);
+            }
+            list.addClass('twittersearch-list');
+            console.log(data);
+            
+            
+            $(data.results).each(function(i, twitt) {
+                $('<li>')
+                    .html(twitt.text)
+                    .appendTo(list);
+            });
+        });
+    }
     
 })(jQuery);
